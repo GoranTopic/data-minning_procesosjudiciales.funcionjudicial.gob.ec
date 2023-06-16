dafc51a74705c544de065d37f39ae5e84d7b6718
Goran Topic <telegonicaxx@live.com>
2023-06-15 18:22:43 -0700

    removed node_modules

--- src/utils/hasSymbols.js
+++
@@ -1,5 +0,0 @@
-// make a fucntion that return true if a given string any symbols
-let hasSymbols = 
-    string => /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+/.test(string)
-
-export default hasSymbols
