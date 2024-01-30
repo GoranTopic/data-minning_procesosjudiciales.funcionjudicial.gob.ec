e29d5222c1151984d4262f4d7a1743a56592e16b
Goran Topic <telegonicaxx@live.com>
2024-01-30 15:31:55 -0500

    updates api

--- src/api/endpoints.js
+++ src/api/endpoints.js
@@ -1,4 +1,4 @@
-let api_endpoint = 'https://api.funcionjudicial.gob.ec/informacion/';
+let api_endpoint = 'https://api.funcionjudicial.gob.ec/EXPEL-CONSULTA-CAUSAS-SERVICE/api/consulta-causas/informacion/';
 
 let domain = 'https://procesosjudiciales.funcionjudicial.gob.ec/expel-busqueda-avanzada';
 
